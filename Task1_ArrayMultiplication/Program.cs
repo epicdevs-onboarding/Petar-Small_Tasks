cc3bf4bffea3c118ea0561a33fb996173e83e056
petar-ang <petar.angelov@epicdevs.io>
2022-12-07 12:23:23 +0200

    Add enum for rounding options

--- Task1_ArrayMultiplication/Program.cs
+++ Task1_ArrayMultiplication/Program.cs
@@ -5,6 +5,7 @@ namespace Task1_ArrayMultiplication
 {
     internal class Program
     {
+        const int DIGITS_AFTER_FLOATING_POINT = 2;
         static void Main(string[] args)
         {
             
@@ -31,21 +32,25 @@ namespace Task1_ArrayMultiplication
                 }
             } while (!int.TryParse(input2, out maxValue));
 
-            int roundingOption;
+            RoundingOption option = new RoundingOption();
             do
             {
-                Console.WriteLine("Choose a desired rounding option for the results: \n (1) ceiling() \n (2) floor() \n (3) Math.Round() ");
+                Console.WriteLine("Choose a desired rounding option for the results: \n (1) Round the result to the higher number of the hundreds (Ceiling) " +
+                                                                                    "\n (2) Round the result to the lower number of the hundreds (Floor) " +
+                                                                                    "\n (3) Round the result to the nearest even number of the hundreds ");
                 input3 = Console.ReadLine();
-                if (!int.TryParse(input3, out roundingOption) || !(roundingOption >= 1 && roundingOption <= 3))
+                if (!Enum.IsDefined(typeof(RoundingOption), Convert.ToInt32(input3)))
                 {
                     Console.WriteLine("Please enter a valid number between 1 and 3!");
                 }
-            } while (!int.TryParse(input3, out roundingOption) || !(roundingOption >=1 && roundingOption <= 3));
-            
+            } while (!Enum.IsDefined(typeof(RoundingOption), Convert.ToInt32(input3)));
+
+            option = (RoundingOption)Convert.ToInt32(input3);
+
             int[] intArray = GenerateRandomIntArr(arrLength, maxValue);
             decimal[] decimalArray = GenerateRandomDecimalArr(arrLength, maxValue);
             
-            PrintReadableExpression(intArray, decimalArray, roundingOption);
+            PrintReadableExpression(intArray, decimalArray, option);
         }
 
         static int[] GenerateRandomIntArr(int elemCount, int maxValue)
@@ -64,7 +69,7 @@ namespace Task1_ArrayMultiplication
             Random r = new Random();
             for (int i = 0; i < elemCount; i++)
             {
-                decimalArray[i] = Math.Round(Convert.ToDecimal(r.NextDouble()) * maxValue, 3);
+                decimalArray[i] = Math.Round((decimal)r.NextDouble() * maxValue, 3);
             }
             return decimalArray;
         }
@@ -76,22 +81,41 @@ namespace Task1_ArrayMultiplication
             return Convert.ToDecimal(rounded);
         }
 
+        static decimal MathRound(decimal d) => Math.Round(d, DIGITS_AFTER_FLOATING_POINT);
+
         static decimal RoundCeiling(decimal d)
         {
             return RoundFloor(d) + 0.01m;
         }
-        static void PrintReadableExpression(int[] intArr, decimal[] decimalArr, int roundingOption)
+        static void PrintReadableExpression(int[] intArr, decimal[] decimalArr, RoundingOption opt)
         {
             for (int i = 0; i < intArr.Length; i++)
-            {   
-                if (roundingOption == 1)
-                    Console.WriteLine($"{intArr[i]} x {decimalArr[i]} = {RoundCeiling(decimalArr[i] * intArr[i])}");
-                else if (roundingOption == 2)
-                    Console.WriteLine($"{intArr[i]} x {decimalArr[i]} = {RoundFloor(decimalArr[i] * intArr[i])}");
-                else
-                    Console.WriteLine($"{intArr[i]} x {decimalArr[i]} = {Math.Round(decimalArr[i] * intArr[i], 2)}");
-
+            {
+                switch (opt)
+                {
+                    case RoundingOption.CeilingRounding:
+                    {
+                        PrintExpression(intArr[i], decimalArr[i], RoundCeiling);
+                        break;
+                    }
+                    case RoundingOption.FloorRounding:
+                    {
+                        PrintExpression(intArr[i], decimalArr[i], RoundFloor);
+                        break;
+                    }
+                    case RoundingOption.DefaultRounding:
+                    {
+                        PrintExpression(intArr[i], decimalArr[i], MathRound);
+                        break;
+                    }
+                }
             }
         }
+
+        static void PrintExpression(int i, decimal d, Func<decimal, decimal> func)
+        {
+            Console.WriteLine($"{i} x {d} = {func(d * i)}");
+        }
     }
+    
 }
